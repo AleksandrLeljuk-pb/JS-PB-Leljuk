0b6ffbaad58483e9776b4d256c7fe1aa9485b20c
AleksandrLeljuk-pb <92275317+AleksandrLeljuk-pb@users.noreply.github.com>
2022-01-18 21:46:00 +0200

    Task-12

--- HW1/Task-12.js
+++ HW1/Task-12.js
@@ -14,7 +14,8 @@
 var arr = [1,2,3,-5,-2,1,-4];
 var arrSumEven = 0;
 for (var i=0; i < arr.length; i++) {
-    if( (arr[i] > 3) && (arr[i] < 10) ) {
-        console.log(arr[i]);
+    if( arr[i] > 0)  {
+        arrSumEven += arr[i];
     } 
 }
+console.log(arrSumEven)
